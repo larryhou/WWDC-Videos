5b7f68d5ddf49086155561b6e42ad41ae0b42e95
larryhou <larryhou@foxmail.com>
2015-06-11 11:11:38 +0800

    WWDC 2015 Videos tool

---
+++ WWDC2015.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+dir=./Videos
+while getopts :d:h OPTION
+do
+	case ${OPTION} in
+		d) dir=${OPTARG};;
+		h) echo "Usage: $(basename $0) -d [VIDEO_OUTPUT_DIR] -h [HELP]"
+		   exit;;
+		:) echo "ERR: -${OPTARG} 缺少参数, 详情参考: $(basename $0) -h" 1>&2
+		   exit 1;;
+		?) echo "ERR: 输入参数-${OPTARG}不支持, 详情参考: $(basename $0) -h" 1>&2
+		   exit 1;;
+	esac
+done
+
+base=https://developer.apple.com/videos/wwdc/2015/
+curl -s ${base} | grep 'href=\"?id=[0-9]*\"'     \
+	| awk -F'href' '{print $2}' | awk -F\" '{print $2}' | while read id
+do
+	echo "${base}${id}"
+	curl -s ${base}${id} | sed 's/\(http:\/\/[^\"]*\)/<<\1>>/g' | sed $'s/<</\\\n/g' \
+		| grep 'http://devstreaming.apple.com' | awk -F'>>' '{print $1}'             \
+		| grep '_hd_' | while read url
+	do
+		name=$(echo ${url} | awk -F/ '{print $NF}' | awk -F? '{print $1}')
+		if [ -f "${dir}/${name}.f" ]
+		then
+			continue
+		fi
+		
+		axel -a ${url} -o ${dir}/${name}
+		if [ $? -eq 0 ]
+		then
+			touch ${dir}/${name}.f
+		fi
+	done
+done
